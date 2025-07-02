f4a501ad2b5e8023a955dc17efce62620960b925
Sandu Guzun <sanduguzun762@gmail.com>
2025-07-02 20:28:37 +0300

    Chore: Add new List of Users in UserController

--- src/main/java/com/example/thymeleaf_tutorial/controller/UserController.java
+++ src/main/java/com/example/thymeleaf_tutorial/controller/UserController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
 
@@ -17,6 +20,7 @@ public class UserController {
 
     }
 
+
     //handler method to handle selection expression
     @GetMapping("selection-expression")
     public String selectionExpression(Model model) {
@@ -24,5 +28,40 @@ public class UserController {
         model.addAttribute("user", user);
         return "selection-expression";
     }
+
+    // handler method to handle message expression request
+    @GetMapping("/message-expression")
+    public String messageExpression() {
+        return "message-expression";
+    }
+
+    // handler method to handle link expression
+    @GetMapping("/link-expression")
+    public String linkExpression(Model model){
+        model.addAttribute("id", 1);
+        return "link-expression";
+    }
+
+    // handler method to handle fragment expression
+    @GetMapping("/fragment-expression")
+    public String fragmentExpression(){
+        return "fragment-expression";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        User admin = new User("Admin", "admin@gmail.com", "ADMIN", "Male");
+        User sandu = new User("Sandu", "sandug@gmail.com", "Owner", "Male");
+        User catalina = new User("Catalina", "catalina@gmail.com", "Administrator", "Female");
+        List<User> users = new ArrayList<>();
+        users.add(sandu);
+        users.add(catalina);
+        users.add(admin);
+        model.addAttribute("users", users);
+        return "users";
+
+    }
+
+
 
 }
